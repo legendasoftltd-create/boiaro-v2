3041d713afe6861e1d885f7ea59229450d795510
unknown <rakibmail1997@gmail.com>
2026-04-26 00:29:48 +0600

    feat:footer api

--- server/src/services/footer.service.ts
+++ server/src/services/footer.service.ts
@@ -1,14 +1,8 @@
 import { prisma } from "../lib/prisma.js";
 
-export const getFooterData = async(req, res)=>{
-    
-
-    const footerData = await prisma.siteSetting.findMany({
-        
-        
-    });
-
-    return{
-        footerData
-    }
-}
+export const getFooterData = async () => {
+  const footerData = await prisma.siteSetting.findMany({});
+  return {
+    footerData,
+  };
+};
